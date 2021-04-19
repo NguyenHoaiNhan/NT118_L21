f23ab12597688d03888c8d2bdb45c4aa70372b91
DangTH112001 <81699999+DangTH112001@users.noreply.github.com>
2021-04-19 10:05:39 +0700

    Revert "Thêm tính năng Login"

--- app/src/main/java/com/kane/healthapp/MainActivity.java
+++ app/src/main/java/com/kane/healthapp/MainActivity.java
@@ -2,7 +2,6 @@ package com.kane.healthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
